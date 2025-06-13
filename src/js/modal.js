51b5a9e0020b255112b0e2f9d5cf89fd2f52b774
AlyonaLutsyk <148708226+AlyonaLutsyk@users.noreply.github.com>
2025-06-13 11:40:33 +0300

    modal-fix

--- src/js/modal.js
+++ src/js/modal.js
@@ -17,4 +17,19 @@
     refs.modal.classList.remove('is-open');
     document.body.classList.remove('modal-open');
   }
+  document.querySelectorAll('.modal-list a').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1); 
+      const targetSection = document.getElementById(targetId);
+
+      closeModal();
+
+      setTimeout(() => {
+        if (targetSection) {
+          targetSection.scrollIntoView({ behavior: 'smooth' });
+        }
+      }, 300); 
+    });
+  });
 })();
